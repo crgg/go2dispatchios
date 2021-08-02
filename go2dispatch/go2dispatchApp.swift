70b13e6ade97ba7402941aeab7a0e050233f7060
Ramon Gajardo <crgg@sbcglobal.net>
2021-08-02 17:29:45 -0500

    Initial Commit

---
+++ go2dispatch/go2dispatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  go2dispatchApp.swift
+//  go2dispatch
+//
+//  Created by Ramon Gajardo on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct go2dispatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
